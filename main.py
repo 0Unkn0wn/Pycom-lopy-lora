9310dddc0c72de8570705e87e0edb6fb9bfaf407
0Unkn0wn <mihnea.bastea@gmail.com>
2022-11-29 12:38:07 +0100

    modified the code for easy modification

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ import pycom
 from pycoproc_1 import Pycoproc
 import machine
 
-# from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
 from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
 
+app_eui = '0000000000000000' # leave as default even in ttn
+app_key = 'EA83E838C98E5A3B61D416B57EE508D6' # change this to change the ttn app
+
+#offset values used to make the sensor print correct values
+offset_value = -7
+offset_procentage = 0.75
+
+#reboot settings
 pycom.heartbeat(False)
 pycom.rgbled(0x0A0A08) # white
 
 py = Pycoproc(Pycoproc.PYSENSE)
 
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-app_eui = ubinascii.unhexlify('0000000000000000')
-app_key = ubinascii.unhexlify('EA83E838C98E5A3B61D416B57EE508D6')
+app_eui = ubinascii.unhexlify(app_eui)
+app_key = ubinascii.unhexlify(app_key)
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 while not lora.has_joined():
     pycom.rgbled(0xFF0000) #red
@@ -35,19 +42,14 @@ while lora.has_joined():
     pycom.rgbled(0x00FF00) #green
     mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa. Mode may also be set to ALTITUDE, returning a value in meters
     si = SI7006A20(py)
-    # print("Temperature: " + str(si.temperature())+ " deg C and Relative Humidity: " + str(si.humidity()) + " %RH")
-    # print("Dew point: "+ str(si.dew_point()) + " deg C")
-    t_ambient = 22
-    # print("Humidity Ambient for " + str(t_ambient) + " deg C is " + str(si.humid_ambient(t_ambient)) + "%RH")
     lt = LTR329ALS01(py)
-    # print("Light (channel Blue lux, channel Red lux): " + str(lt.light()))
-    temp=si.temperature()
+    # temp=si.temperature()
     hum=si.humidity()
     press=mpp.pressure()
     li=lt.light()
     #temp with offset comment both lines and change the payload and temppack to go to the ones without the offset
-    temp_with_offset = (si.temperature()-7)
-    temp_with_offset = (si.temperature()*0.75)
+    temp_with_offset = (si.temperature()+offset_value)
+    temp_with_offset = (si.temperature()*offset_procentage)
     payload = '{"Temperature":'+str(temp_with_offset)+',"Humidity":'+str(hum)+',"Pressure":'+str(press)+',"Light_index":'+str(li)+'}'
     print(payload)
     temppack = ustruct.pack('f',temp_with_offset)
@@ -55,8 +57,5 @@ while lora.has_joined():
     presspack = ustruct.pack('f',press)
     lippack = ustruct.pack('2f',*li)
     payloadpack = temppack+humpack+presspack+lippack
-    # testing the packet
-    # print("Packed Send_packet is:", payloadpack)
-    # print("Unpacked Send_packet is:", ustruct.unpack('<5f',payloadpack))
     s.send(payloadpack)
     time.sleep(6)
