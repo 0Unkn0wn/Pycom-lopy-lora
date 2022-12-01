d78d579a90559264dfcf161599636f1710f1fac6
0Unkn0wn <mihnea.bastea@gmail.com>
2022-12-01 11:38:43 +0100

    modified the code with documentation and func

--- main.py
+++ main.py
@@ -11,29 +11,82 @@ from SI7006A20 import SI7006A20
 from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
 
-app_eui = '0000000000000000' # leave as default even in ttn
-app_key = '4DB0377D6A9278786CE27B70688B2419' # change this to change the ttn app
+
+def led_color(hex_color):
+    ##    The function that sets the color of the led.
+    #    @param hex color - The color the led will have but in hexadecimal
+    #
+    pycom.rgbled(hex_color)
+
+
+def make_lora_connection(app_eui,app_key):
+    ##    The function that makes the LoRa connection
+    #    @param app_eui: The standard app_eui provided by TTN documentation. Should not be changed.
+    #    @param app_key: The key provided by TTN when a device is added in a TTN application.
+    #
+    lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+    app_eui = ubinascii.unhexlify(app_eui)
+    app_key = ubinascii.unhexlify(app_key)
+    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+    return lora
+
+
+def print_payload(payloadpack):
+    ##    The function that prints how the payload will look like in the console.
+    #
+    payload_unpacked=ustruct.unpack("<4f",payloadpack)
+    print("Temperature,Humidity,Pressure,Light_index\n",payload_unpacked)
+
+def construct_payload(si,mpp,li):
+    ##    The function that constructs the payload to be ready for sending.
+    #    @param si: The variable for the temperature and humidity sensor.
+    #    @param mpp: The variable for the pressure sensor.
+    #    @param li: The variable for the light sensor
+    #    @return: The function returns the constructed payload already in bit format ready to be sent to the TTN console.
+    #
+
+    #Default value of the temperature sensor reading.
+    #temp=si.temperature()
+    hum=si.humidity()
+    press=mpp.pressure()
+    li=lt.lux()
+    #temp with offset comment both lines and change the payload and temppack to go to the ones without the offset
+    temp_with_offset = (si.temperature()+OFFSET_VALUE)
+    temp_with_offset = (si.temperature()*OFFSET_PROCENTAGE)
+    # DEBUG:
+    # return '{"Temperature":'+str(temp_with_offset)+',"Humidity":'+str(hum)+',"Pressure":'+str(press)+',"Light_index":'+str(li)+'}'
+    #Packing the data into bytes so it is be sent in a package.
+    temppack = ustruct.pack('f',temp_with_offset)
+    humpack = ustruct.pack('f',hum)
+    presspack = ustruct.pack('f',press)
+    lippack = ustruct.pack('f',li)
+    #Making the final package.
+    payloadpack = temppack+humpack+presspack+lippack
+    return payloadpack
+
+
+APP_EUI = '0000000000000000' # leave as default even in ttn
+APP_KEY = '4DB0377D6A9278786CE27B70688B2419' # change this to change the ttn app
 
 #offset values used to make the sensor print correct values
-offset_value = -7
-offset_procentage = 0.75
+OFFSET_VALUE = -7
+OFFSET_PROCENTAGE = 0.75
+SEND_DELAY = 60
+
 
 #reboot settings
 pycom.heartbeat(False)
-pycom.rgbled(0x0A0A08) # white
+led_color(0x0A0A08) # white
 
 py = Pycoproc(Pycoproc.PYSENSE)
 
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-app_eui = ubinascii.unhexlify(app_eui)
-app_key = ubinascii.unhexlify(app_key)
-lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+lora = make_lora_connection(APP_EUI,APP_KEY)
 ##
 # Looping until it connects to ttn.
 # Red until the loop is done green when it connects.
 #
 while not lora.has_joined():
-    pycom.rgbled(0xFF0000) #red
+    led_color(0xFF0000) #red
     print('Not yet joined...')
     time.sleep(2.5)
 
@@ -41,7 +94,7 @@ print('Joined')
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 s.setblocking(True)
-pycom.rgbled(0x00FF00) #green
+led_color(0x00FF00) #green
 time.sleep(4)
 
 ##
@@ -50,31 +103,16 @@ time.sleep(4)
 #
 try:
     while lora.has_joined():
-        pycom.rgbled(0x000000)
-        mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa. Mode may also be set to ALTITUDE, returning a value in meters.
-        si = SI7006A20(py)#Temperature sensor.
+        led_color(0x000000)#Led off
+        mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa
+        si = SI7006A20(py)#Temperature sensor
         lt = LTR329ALS01(py)#Light sensor
-        #Default value of the sensor reading.
-        #temp=si.temperature()
-        hum=si.humidity()
-        press=mpp.pressure()
-        li=lt.lux()
-        #temp with offset comment both lines and change the payload and temppack to go to the ones without the offset
-        temp_with_offset = (si.temperature()+offset_value)
-        temp_with_offset = (si.temperature()*offset_procentage)
-        #How the payload will look like in the ttn console
-        payload = '{"Temperature":'+str(temp_with_offset)+',"Humidity":'+str(hum)+',"Pressure":'+str(press)+',"Light_index":'+str(li)+'}'
-        print(payload)
-        #Packing the data into bytes so it is be sent in a package.
-        temppack = ustruct.pack('f',temp_with_offset)
-        humpack = ustruct.pack('f',hum)
-        presspack = ustruct.pack('f',press)
-        lippack = ustruct.pack('f',li)
         #Making the final package.
-        payloadpack = temppack+humpack+presspack+lippack
+        payloadpack = construct_payload(si,mpp,lt)
+        #How the payload will look like in the ttn console
+        print_payload(payloadpack)
         #Sending the packet
         s.send(payloadpack)
-        #Modify this to change how fast the data is sent.
-        time.sleep(60)
+        time.sleep(SEND_DELAY)
 except:
     machine.reset()
