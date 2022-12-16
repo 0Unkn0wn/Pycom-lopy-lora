fef5a167023f242212d4de1b36a87c8040719e80
0Unkn0wn <mihnea.bastea@gmail.com>
2022-12-16 12:20:07 +0100

    updated the code
    modified the comments so they are doxygen style
    added the fuction to calculate the luminance
    added the voltage and sent it to ttn asw
    modified the data so it's like the other sensors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import ustruct
 import pycom
 from pycoproc_1 import Pycoproc
 import machine
-
+import math
 from SI7006A20 import SI7006A20
 from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
@@ -16,24 +16,33 @@ from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
 APP_EUI = '0000000000000000' # leave as default even in ttn
 APP_KEY = '4DB0377D6A9278786CE27B70688B2419' # change this to change the ttn app
 
-#offset values used to make the sensor print correct values
+#offset values used to make the sensors print correct values
 HUMIDITY_OFFSET_PERCENTAGE = 0.595
 TEMPERATURE_OFFSET_PERCENTAGE = 0.718
 SEND_DELAY = 240
 
 
 def led_color(hex_color):
-    ##    The function that sets the color of the led.
-    #    @param hex color - The color the led will have but in hexadecimal
-    #
+    """
+    Sets the color of the LED to the specified hex color.
+
+    @param hex_color: The hexadecimal color to set the LED to.
+    @type hex_color: str
+    """
     pycom.rgbled(hex_color)
 
 
 def make_lora_connection(app_eui,app_key):
-    ##    The function that makes the LoRa connection
-    #    @param app_eui: The standard app_eui provided by TTN documentation. Should not be changed.
-    #    @param app_key: The key provided by TTN when a device is added in a TTN application.
-    #
+    """
+    Makes a LoRa connection using the given app EUI and app key.
+
+    @param app_eui: The app EUI to use for the connection.
+    @type app_eui: str
+    @param app_key: The app key to use for the connection.
+    @type app_key: str
+    @return: The LoRa object representing the connection.
+    @rtype: LoRa
+    """
     lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
     app_eui = ubinascii.unhexlify(app_eui)
     app_key = ubinascii.unhexlify(app_key)
@@ -42,25 +51,46 @@ def make_lora_connection(app_eui,app_key):
 
 
 def print_payload(payloadpack):
-    ##    The function that prints how the payload will look like in the console.
-    #
+    """
+    Prints the values of temperature, humidity, pressure, light index, and battery voltage contained in a given payloadpack.
+
+    @param payloadpack: The payloadpack containing the values to be printed.
+    @type payloadpack: bytes
+    """
     payload_unpacked=ustruct.unpack("ffiif",payloadpack)
     print("Temperature,Humidity,Pressure,Light_index,Battery_voltage\n",payload_unpacked)
 
-def construct_payload(si,mpp,li):
-    ##    The function that constructs the payload to be ready for sending.
-    #    @param si: The variable for the temperature and humidity sensor.
-    #    @param mpp: The variable for the pressure sensor.
-    #    @param li: The variable for the light sensor
-    #    @return: The function returns the constructed payload already in
-    #             bit format ready to be sent to the TTN console.
-    #
+def calculate_luminance(light):
+    """
+    Calculates the luminance value of a given light intensity.
 
-    #Default value of the temperature sensor reading.
+    @param light: The light intensity to calculate the luminance for.
+    @type light: int
+    @return: The calculated luminance value.
+    @rtype: int
+    """
+    if(light < 123):
+        return int(round(light))
+    else:
+        light = int(round(math.log(light,1.04),0))
+        if(light > 255):
+            light = 255
+        return light
+
+def construct_payload(si,mpp,li):
+    """
+    Constructs a payload containing the values of temperature, humidity, pressure, light index, and battery voltage.
+
+    @param si: The object representing the temperature and humidity sensor.
+    @param mpp: The object representing the pressure sensor.
+    @param lt: The object representing the light sensor.
+    @return: The constructed payload containing the values of temperature, humidity, pressure, light index, and battery voltage.
+    @rtype: bytes
+    """
     temp = round(si.temperature() * TEMPERATURE_OFFSET_PERCENTAGE,2)
     hum = round(si.humidity() * HUMIDITY_OFFSET_PERCENTAGE,1)
     press = int(round(mpp.pressure()/100)) # mbar conversion
-    li = int(round(lt.lux()))
+    li = calculate_luminance(lt.lux())
     volatage = round(py.read_battery_voltage(),2)
     # DEBUG:
     # return '{"Temperature":'+str(temp)+',"Humidity":'+str(hum)+',"Pressure":'+str(press)+',"Light_index":'+str(li)+'}'
@@ -76,13 +106,14 @@ led_color(0x0A0A08) # white
 py = Pycoproc(Pycoproc.PYSENSE)
 
 lora = make_lora_connection(APP_EUI,APP_KEY)
-##
-# Looping until it connects to ttn.
-# Red until the loop is done green when it connects.
-#
+
+# Loop until the LoRa connection has been established
 while not lora.has_joined():
-    led_color(0xFF0000) #red
+    # Set the LED color to red
+    led_color(0xFF0000)
+    # Print a message indicating that the connection is not yet established
     print('Not yet joined...')
+    # Sleep for 2.5 seconds before checking the connection again
     time.sleep(2.5)
 
 print('Joined')
@@ -92,10 +123,9 @@ s.setblocking(True)
 led_color(0x00FF00) #green
 time.sleep(4)
 
-##
+
 # Looping so it sends the sensor readings.
 # Green when it is connected then off so it doesn't affect the lux calculations.
-#
 try:#everyting under a try except so if the i2c bus fails the machine resets.
     while lora.has_joined():
         led_color(0x000000)#Led off
